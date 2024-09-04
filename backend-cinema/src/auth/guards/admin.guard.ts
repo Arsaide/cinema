8647828b909e700737ed797cca40e857a714a49e
Arsaide <arsaidekm@gmail.com>
2024-09-05 00:12:17 +0300

    added guards
    - added jwt guards and admin guards for safety requests

---
+++ backend-cinema/src/auth/guards/admin.guard.ts
@@ -0,0 +1,15 @@
+import { CanActivate, ExecutionContext, ForbiddenException, Injectable } from '@nestjs/common';
+import { User, UserRole } from '@prisma/client';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+    canActivate(context: ExecutionContext): boolean {
+        const request = context.switchToHttp().getRequest<{ user: User }>();
+        const user = request.user;
+
+        if (user.role !== UserRole.ADMIN)
+            throw new ForbiddenException("You don't have permissions to perform this operation!");
+
+        return true;
+    }
+}
