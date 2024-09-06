d63436161370363f0def7213b6eb68fdce1b7344
Arsaide <arsaidekm@gmail.com>
2024-09-07 00:52:25 +0300

    worked with upload file request
    - added upload file request
    - added view file request

--- backend-cinema/src/file/file.controller.ts
+++ backend-cinema/src/file/file.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpCode, Post, Query, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileService } from './file.service';
+import { Auth } from '../auth/decorators/auth.decorator';
+import { FileInterceptor } from '@nestjs/platform-express';
 
-@Controller('file')
+@Controller('files')
 export class FileController {
-  constructor(private readonly fileService: FileService) {}
+    constructor(private readonly fileService: FileService) {}
+
+    @Post()
+    @HttpCode(200)
+    @Auth('admin')
+    @UseInterceptors(FileInterceptor('image'))
+    async uploadFile(@UploadedFile() file: Express.Multer.File, @Query('folder') folder?: string) {
+        return this.fileService.saveFiles([file], folder);
+    }
 }
