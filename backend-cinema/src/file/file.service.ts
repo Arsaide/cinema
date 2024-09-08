1c6e0a1aad7953276cd087177c02c64d08b4228a
Arsaide <arsaidekm@gmail.com>
2024-09-08 18:37:24 +0300

    worked with unique file name

--- backend-cinema/src/file/file.service.ts
+++ backend-cinema/src/file/file.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { FileResponse } from './file.interface';
 import { path } from 'app-root-path';
 import { ensureDir, writeFile } from 'fs-extra';
+import * as crypto from 'crypto';
+import * as dayjs from 'dayjs';
 
 @Injectable()
 export class FileService {
@@ -15,11 +17,13 @@ export class FileService {
 
         const res: FileResponse[] = await Promise.all(
             files.map(async file => {
-                await writeFile(`${uploadFolder}/${file.originalname}`, file.buffer);
+                const uniqueFileName = `${dayjs().format('DD-MM-YYYY')}-${crypto.randomBytes(4).toString('hex')}-${file.originalname}`;
+
+                await writeFile(`${uploadFolder}/${uniqueFileName}`, file.buffer);
 
                 return {
-                    url: `/uploads/${folder}/${file.originalname}`,
-                    name: file.originalname,
+                    url: `/uploads/${folder}/${uniqueFileName}`,
+                    name: uniqueFileName,
                 };
             }),
         );
