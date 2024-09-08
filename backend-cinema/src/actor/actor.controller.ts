fc52f46131a2eaeb052e19ef1119bc8fbb67cbe1
Arsaide <arsaidekm@gmail.com>
2024-09-08 21:24:29 +0300

    added new actors services
    - added create actor request
    - added edit actor request
    - added delete actor request
    - added get by id and slug request
    - added get all actors request

--- backend-cinema/src/actor/actor.controller.ts
+++ backend-cinema/src/actor/actor.controller.ts
@@ -1,7 +1,70 @@
-import { Controller } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    HttpCode,
+    NotFoundException,
+    Param,
+    Post,
+    Put,
+    Query,
+    UsePipes,
+    ValidationPipe,
+} from '@nestjs/common';
 import { ActorService } from './actor.service';
+import { Auth } from '../auth/decorators/auth.decorator';
+import { UpdateActorDto } from './dto/update-actor.dto';
 
-@Controller('actor')
+@Controller('actors')
 export class ActorController {
-  constructor(private readonly actorService: ActorService) {}
+    constructor(private readonly actorService: ActorService) {}
+
+    @Get()
+    async getAll(@Query('searchTerm') searchTerm?: string) {
+        return this.actorService.getAll(searchTerm);
+    }
+
+    @Get('by-slug/:slug')
+    async getBySlug(@Param('slug') slug: string) {
+        return this.actorService.getBySlug(slug);
+    }
+
+    /* Admin requests */
+
+    @Get('by-id/:id')
+    @Auth('admin')
+    async getById(@Param('id') id: string) {
+        return this.actorService.getById(id);
+    }
+
+    @UsePipes(new ValidationPipe())
+    @Post()
+    @HttpCode(200)
+    @Auth('admin')
+    async create() {
+        return this.actorService.create();
+    }
+
+    @UsePipes(new ValidationPipe())
+    @Put(':id')
+    @HttpCode(200)
+    @Auth('admin')
+    async update(@Param('id') id: string, @Body() dto: UpdateActorDto) {
+        const updatedActor = await this.actorService.update(id, dto);
+
+        if (!updatedActor) throw new NotFoundException('Actor not found!');
+
+        return updatedActor;
+    }
+
+    @Delete(':id')
+    @Auth('admin')
+    async delete(@Param('id') id: string) {
+        const deletedActor = await this.actorService.delete(id);
+
+        if (!deletedActor) throw new NotFoundException('Actor not found!');
+
+        return deletedActor;
+    }
 }
---
+++ backend-cinema/src/actor/return-actor.object.ts
@@ -0,0 +1,9 @@
+import { Prisma } from '@prisma/client';
+
+export const returnActorObject: Prisma.ActorSelect = {
+    id: true,
+    createdAt: true,
+    name: true,
+    slug: true,
+    photoUrl: true,
+};
