e87c262150c2782ec1e45d1cf071d104aa8e348f
Arsaide <arsaidekm@gmail.com>
2024-09-10 23:24:56 +0300

    convert images to webp
    - convert images to webp
    - refactoring code

--- backend-cinema/src/file/file.service.ts
+++ backend-cinema/src/file/file.service.ts
@@ -4,6 +4,7 @@ import { path } from 'app-root-path';
 import { ensureDir, writeFile } from 'fs-extra';
 import * as crypto from 'crypto';
 import * as dayjs from 'dayjs';
+import * as sharp from 'sharp';
 
 @Injectable()
 export class FileService {
@@ -18,12 +19,23 @@ export class FileService {
         const res: FileResponse[] = await Promise.all(
             files.map(async file => {
                 const uniqueFileName = `${dayjs().format('DD-MM-YYYY')}-${crypto.randomBytes(4).toString('hex')}-${file.originalname}`;
+                const fileExtension = file.originalname.split('.').pop();
+                const baseFileName = uniqueFileName.replace(/\.[^/.]+$/, '');
+                let fileName = `${baseFileName}.${fileExtension}`;
+                let filePath = `${uploadFolder}/${fileName}`;
 
-                await writeFile(`${uploadFolder}/${uniqueFileName}`, file.buffer);
+                if (file.mimetype.startsWith('image/')) {
+                    fileName = `${fileName}.webp`;
+                    filePath = `${uploadFolder}/${fileName}`;
+
+                    await sharp(file.buffer).webp({ quality: 90 }).toFile(filePath);
+                } else {
+                    await writeFile(filePath, file.buffer);
+                }
 
                 return {
-                    url: `/uploads/${folder}/${uniqueFileName}`,
-                    name: uniqueFileName,
+                    url: `/uploads/${folder}/${fileName}`,
+                    name: fileName,
                 };
             }),
         );
