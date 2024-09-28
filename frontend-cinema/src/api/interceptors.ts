e2931854dbba14e51a3fa79697c8ca0f437dfb9c
Arsaide <arsaidekm@gmail.com>
2024-09-28 15:47:54 +0300

    worked with api
    - added error catcher config
    - added auth service and settings him
    - worked with axios interceptors and auth

---
+++ frontend-cinema/src/api/interceptors.ts
@@ -0,0 +1,53 @@
+import axios, { type CreateAxiosDefaults } from 'axios';
+
+import { errorCatch } from '@/api/error';
+import { getAccessToken, removeFromStorage } from '@/api/services/auth/auth-token.service';
+import authService from '@/api/services/auth/auth.service';
+
+import { SERVER_URL } from '@/config/api.config';
+
+const options: CreateAxiosDefaults = {
+	baseURL: SERVER_URL,
+	headers: {
+		'Content-Type': 'application/json',
+	},
+};
+
+const axiosClassic = axios.create(options);
+const axiosWithAuth = axios.create(options);
+
+axiosWithAuth.interceptors.request.use(config => {
+	const accessToken = getAccessToken;
+
+	if (config && config.headers && accessToken)
+		config.headers.Authorization = `Bearer ${accessToken()}`;
+
+	return config;
+});
+
+axiosWithAuth.interceptors.response.use(
+	config => config,
+	async error => {
+		const originalRequest = error.config;
+
+		if (
+			(error.response.status === 401 ||
+				errorCatch(error) === 'jwt expired' ||
+				errorCatch(error) === 'jwt must be provided') &&
+			error.config &&
+			!error.config._isRetry
+		) {
+			originalRequest._isRetry = true;
+			try {
+				await authService.getNewTokens();
+				return axiosWithAuth.request(originalRequest);
+			} catch (error) {
+				if (errorCatch(error) === 'jwt expired') removeFromStorage();
+			}
+		}
+
+		throw error;
+	},
+);
+
+export { axiosClassic, axiosWithAuth };
---
+++ frontend-cinema/src/api/services/auth/auth.service.ts
@@ -0,0 +1,33 @@
+import Cookies from 'js-cookie';
+
+import { axiosClassic } from '@/api/interceptors';
+import { EnumTokens, saveToStorage } from '@/api/services/auth/auth-token.service';
+
+import { API_URL } from '@/config/api.config';
+
+import { IAuthResponse } from '@/types/auth.types';
+
+class AuthService {
+	async main(type: 'login' | 'register', data: IAuthResponse) {
+		const response = await axiosClassic.post<IAuthResponse>(API_URL.auth(`/${type}`), data);
+
+		if (response.data.accessToken) saveToStorage(response.data);
+
+		return response;
+	}
+
+	async getNewTokens() {
+		const refreshToken = Cookies.get(EnumTokens.REFRESH_TOKEN);
+
+		const response = await axiosClassic.post<string, { data: IAuthResponse }>(
+			API_URL.auth(`/login/access-token`),
+			{ refreshToken },
+		);
+
+		if (response.data.accessToken) saveToStorage(response.data);
+
+		return response;
+	}
+}
+
+export default new AuthService();
